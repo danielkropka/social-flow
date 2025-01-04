ba3aa7c4c3b7aabf5f8d81feb8a430382865302b
danielkropka <danielwarpechowski9@gmail.com>
2025-01-04 21:47:16 +0100

    .

--- components/PlatformsSection.tsx
+++ components/PlatformsSection.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { motion } from "framer-motion";
 import { Facebook, Instagram, Twitter, Linkedin } from "lucide-react";
 
 export default function PlatformsSection() {
