56cc699d13bbc67a31a8ecaadc994e49583369ec
danielkropka <danielwarpechowski9@gmail.com>
2025-03-21 15:49:03 +0100

    Update route.ts

--- app/api/accounts/[id]/route.ts
+++ app/api/accounts/[id]/route.ts
@@ -2,13 +2,6 @@ import { NextResponse } from "next/server";
 import { ConnectedAccount } from "@prisma/client";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/prisma";
-
-type Props = {
-  params: {
-    id: string;
-  };
-};
-
 // Funkcja do odświeżania tokenu Instagram
 async function refreshInstagramToken(accessToken: string) {
   try {
