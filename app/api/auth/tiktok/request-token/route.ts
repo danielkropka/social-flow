58e7c818753ab563f62f43c220b8650bc2c160d2
danielkropka <danielwarpechowski9@gmail.com>
2025-05-11 19:12:06 +0200

    Update route.ts

--- app/api/auth/tiktok/request-token/route.ts
+++ app/api/auth/tiktok/request-token/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import { getAuthSession } from "@/lib/auth";
 import crypto from "crypto";
-import { cookies } from "next/headers";
 
 export async function GET() {
   try {
