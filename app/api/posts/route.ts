471d880465df642eac05a23fa058791b2aabca98
danielkropka <danielwarpechowski9@gmail.com>
2025-05-14 18:40:14 +0200

    Update route.ts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,13 +1,12 @@
 import { db } from "@/lib/prisma";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
+import { getAuthSession } from "@/lib/auth";
 import { NextResponse } from "next/server";
 import { MediaType } from "@prisma/client";
 
 export async function POST(req: Request) {
   try {
     // Sprawdzenie autoryzacji
-    const session = await getServerSession(authOptions);
+    const session = await getAuthSession();
     if (!session?.user?.id) {
       return NextResponse.json(
         {
@@ -261,7 +260,7 @@ export async function POST(req: Request) {
 
 export async function GET() {
   try {
-    const session = await getServerSession(authOptions);
+    const session = await getAuthSession();
     if (!session?.user?.id) {
       return NextResponse.json(
         {
