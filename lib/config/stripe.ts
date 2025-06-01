9e6d357289cd47446327d06564d584e3d24218fc
danielkropka <danielwarpechowski9@gmail.com>
2025-06-01 21:45:03 +0200

    refactor: stripe webhooks

--- lib/config/stripe.ts
+++ lib/config/stripe.ts
@@ -6,8 +6,8 @@ export const STRIPE_PLANS = {
     monthlyPrice: 30,
     yearlyPrice: 288,
     priceId: {
-      monthly: process.env.NEXT_PUBLIC_STRIPE_BASIC_MONTHLY_ID,
-      yearly: process.env.NEXT_PUBLIC_STRIPE_BASIC_YEARLY_ID,
+      monthly: process.env.NEXT_PUBLIC_STRIPE_BASIC_MONTHLY_PRICE_ID,
+      yearly: process.env.NEXT_PUBLIC_STRIPE_BASIC_YEARLY_PRICE_ID,
     },
     description:
       "Idealne rozwiązanie dla początkujących twórców i małych firm, które chcą efektywnie zarządzać swoją obecnością w social mediach.",
@@ -24,8 +24,8 @@ export const STRIPE_PLANS = {
     monthlyPrice: 60,
     yearlyPrice: 576,
     priceId: {
-      monthly: process.env.NEXT_PUBLIC_STRIPE_CREATOR_MONTHLY_ID,
-      yearly: process.env.NEXT_PUBLIC_STRIPE_CREATOR_YEARLY_ID,
+      monthly: process.env.NEXT_PUBLIC_STRIPE_CREATOR_MONTHLY_PRICE_ID,
+      yearly: process.env.NEXT_PUBLIC_STRIPE_CREATOR_YEARLY_PRICE_ID,
     },
     description:
       "Zaawansowane narzędzie dla profesjonalistów i firm, które chcą maksymalizować zasięgi i engagement swojego contentu.",
