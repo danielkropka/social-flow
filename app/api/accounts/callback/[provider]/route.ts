25e81906260f43715c0161a7eaaa00498a5f0e7d
danielkropka <danielwarpechowski9@gmail.com>
2025-09-16 21:05:13 +0200

    fix depo error

--- app/api/accounts/callback/[provider]/route.ts
+++ app/api/accounts/callback/[provider]/route.ts
@@ -8,16 +8,14 @@ import {db} from "@/lib/config/prisma";
 
 const DASHBOARD_REDIRECT = "/dashboard"
 
-export async function GET(req: Request, { params }: { params: { provider: Provider }}) {
+export async function GET(req: Request, { params }: { params: { provider: string }}) {
     const url = new URL(req.url);
     const searchParams = url.searchParams;
 
-    const providerParam = params.provider;
-    if (!providerParam || !Object.values(Provider).includes(providerParam as Provider)) {
+    const provider = params.provider as Provider;
+    if (!provider || !Object.values(Provider).includes(provider)) {
         return NextResponse.redirect(new URL(`/dashboard?error=unsupported_provider`, url));
     }
-    const provider = providerParam as Provider;
-
 
     const session = await getServerSession(authOptions);
     if (!session?.user?.id) {
